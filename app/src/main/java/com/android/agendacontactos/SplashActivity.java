033bba9591084da9601f6436a915aba0eaa111d7
James <jjcardonao@gmail.com>
2015-10-27 20:54:14 -0500

    preferences manager

--- app/src/main/java/com/android/agendacontactos/SplashActivity.java
+++ app/src/main/java/com/android/agendacontactos/SplashActivity.java
@@ -5,18 +5,30 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import com.android.agendacontactos.preferences.CacheManager;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private CacheManager cacheManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        cacheManager = new CacheManager(this);
+        
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent i;
+                if(cacheManager.isLoggin()){
+                    i = new Intent(SplashActivity.this, MainActivity.class);
+                }else{
+                    i = new Intent(SplashActivity.this, LoginActivity.class);
+                }
+
                 startActivity(i);
+                finish();
             }
         }, 3000);
     }
